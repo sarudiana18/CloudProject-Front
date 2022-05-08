5a293299ccc7b347a5ba3b18beba736036053431
Saru Diana Mirela <diana.mirela123@yahoo.com>
2022-05-08 16:55:01 +0300

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainPage from './javascript/components/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
