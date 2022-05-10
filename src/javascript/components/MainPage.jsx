12caef80ee0bfe47581e635a79c22df56512e989
Saru Diana Mirela <diana.mirela123@yahoo.com>
2022-05-10 18:13:44 +0300

    Add css for Main text

--- src/javascript/components/MainPage.jsx
+++ src/javascript/components/MainPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function MainPage(){
     return (
         <div id="MainPage">
-
+            <h1 className='mt-10 text-blue-600 text-3xl font-bold'>Main Page</h1>
         </div>
     );
 }
